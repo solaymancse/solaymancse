955334500e176f7fef39045c7176c77b4cbddf32
solayman <mdsolaymancse@gmail.com>
2022-04-01 23:14:35 +0600

    initial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{display:"flex",justifyContent:"center",alignItems:"center",textAlign:"center"}}>
+        <h1>Solayman Muhammad | Portfolio Website <br/>Site is under construction !!!!!</h1>
+        
+    </div>
+  )
+}
+
+export default App
